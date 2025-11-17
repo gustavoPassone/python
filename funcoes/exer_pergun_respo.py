f1ef1669b101560d8e08425ebb9995bb94891483
gustavoPassone <gmpassone@gmail.com>
2025-11-17 10:56:25 -0300

    17/11/25

--- funcoes/exer_pergun_respo.py
+++ funcoes/exer_pergun_respo.py
@@ -17,3 +17,34 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+qtd_acertos = 0
+
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+        
+    escolha = input('Escolha uma opção: ')
+    
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+    
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+        
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+                
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou')
+    else:
+        print('Errou')
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
---
+++ funcoes/lambda__sort_sorted.py
@@ -0,0 +1,26 @@
+# Introdução à função lambda (função anônima de uma linha)
+# A função lambda é uma função como qualquer outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo deve ser contido dentro de uma única expressão.
+
+# lista = [4, 25, 3, 26, 28]
+# lista.sort(reverse=True)
+# print(lista)
+
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
